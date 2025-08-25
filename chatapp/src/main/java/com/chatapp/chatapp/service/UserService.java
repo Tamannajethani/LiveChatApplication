ca59ba521227c8ea4cb5638f3f65492acce92705
Tamannajethani <140835838+Tamannajethani@users.noreply.github.com>
2025-08-26 01:40:28 +0530

    Create UserService.java

---
+++ chatapp/src/main/java/com/chatapp/chatapp/service/UserService.java
@@ -0,0 +1,13 @@
+package com.chatapp.chatapp.service;
+
+import  com.chatapp.chatapp.model.User;
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+    User createUser(User user);
+    Optional<User> getUserById(Long id);
+    List<User> getAllUsers();
+    User updateUser(Long id,User user);
+    void deleteUser(Long id);
+}
