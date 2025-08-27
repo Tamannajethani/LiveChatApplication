b417ed3f54b14b20a62a54a0a158a960bf1fd139
Tamannajethani <140835838+Tamannajethani@users.noreply.github.com>
2025-08-28 01:18:07 +0530

    Update User.java

--- chatapp/src/main/java/com/chatapp/chatapp/model/User.java
+++ chatapp/src/main/java/com/chatapp/chatapp/model/User.java
@@ -34,11 +34,11 @@ public class User {
         this.password = password;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
