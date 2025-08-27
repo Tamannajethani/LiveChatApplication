14526819d53433a703a6bd3ad5983d6091e49e98
Tamannajethani <140835838+Tamannajethani@users.noreply.github.com>
2025-08-28 01:19:20 +0530

    Update UserRepository.java

--- chatapp/src/main/java/com/chatapp/chatapp/repository/UserRepository.java
+++ chatapp/src/main/java/com/chatapp/chatapp/repository/UserRepository.java
@@ -3,6 +3,11 @@ import com.chatapp.chatapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User,Long>{
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByUsernameAndPassword(String username,String password);
 }
