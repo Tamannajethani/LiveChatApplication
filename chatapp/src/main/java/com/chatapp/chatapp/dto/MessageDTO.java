19b9390f6d50c271d5292cead688e646e65e19ec
Tamannajethani <140835838+Tamannajethani@users.noreply.github.com>
2025-08-28 01:15:55 +0530

    Update MessageDTO.java

--- chatapp/src/main/java/com/chatapp/chatapp/dto/MessageDTO.java
+++ chatapp/src/main/java/com/chatapp/chatapp/dto/MessageDTO.java
@@ -1,12 +1,18 @@
 package com.chatapp.chatapp.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 
 public class MessageDTO {
     private String sender;
     private String receiver;
     private String content;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime sentAt;
+
+    private String fileUrl;
+    private String fileType;
 
     public MessageDTO(){}
 
@@ -17,6 +23,14 @@ public class MessageDTO {
         this.sentAt = sentAt;
     }
 
+    public MessageDTO(String sender, String receiver, String content, LocalDateTime sentAt,String fileUrl, String fileType) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.content = content;
+        this.sentAt = sentAt;
+        this.fileUrl = fileUrl;
+        this.fileType = fileType;
+    }
 
     public String getReceiver() {
         return receiver;
@@ -48,5 +62,21 @@ public class MessageDTO {
 
     public void setSentAt(LocalDateTime sentAt) {
         this.sentAt = sentAt;
+    }
+
+    public String getFileUrl() {
+        return fileUrl;
+    }
+
+    public void setFileUrl(String fileUrl) {
+        this.fileUrl = fileUrl;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
     }
 }
